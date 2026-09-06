2a7fe2de0b1aa98093a0ef9ea0243f08df11084a
agi <asorhaindocelli@gmail.com>
2026-09-06 15:47:46 +0100

    basics

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/settings")
+def settings():
+    #pi-hole=dns
+    #so it can communicate properly
+    return render_template("settings.html")
+
+if __name__=="__main__":
+    app.run(debig=True)
